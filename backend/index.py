009724a18e5d9145aa4cac6215e9784a729e9a0e
Devon Minor <devon.minor.00@gmail.com>
2022-12-10 12:29:27 -0500

    add Dockerfile for building the app

--- backend/index.py
+++ backend/index.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello World."
+    return jsonify("Hello World.")
+
+def create_app():
+    return app
