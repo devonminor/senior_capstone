7f5d207ca3024c301a92fc7c68795efe8a2b7235
Najib Daoud <>
2022-12-13 02:31:10 -0500

    Improved UI for question input

--- web/pages/QuestionInput.tsx
+++ web/pages/QuestionInput.tsx
@@ -1,6 +1,8 @@
 import Form from 'react-bootstrap/Form'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button';
+import CloseButton from 'react-bootstrap/CloseButton';
+
 
 export default function QuestionInput() {
   return (
@@ -8,14 +10,47 @@ export default function QuestionInput() {
     <div className="inputBlock">
       <div className="row">
         <div className="textArea">
-          <InputGroup className="mb-4">
-            <InputGroup.Text id="question-input">Q:</InputGroup.Text>
-            <Form.Control
-              placeholder="Type question here"
-              aria-label="Question"
-              aria-describedby="question-input"
-            />
-          </InputGroup>
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Multiple Choice
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a className="dropdown-item" href="#">Multiple Choice</a>
+              <a className="dropdown-item" href="#">Free Response</a>
+              <a className="dropdown-item" href="#">Free Drawing</a>
+            </div>
+          </div>
+        </div>
+        <div className="buttonArea1">
+          <CloseButton />
+        </div>
+        <div className="buttonArea2">
+          <Button variant="success">+</Button>
+        </div>
+        <hr className="line"></hr>
+      </div>
+      <div className="row">
+          <h5>Question</h5>
+            <InputGroup className="mb-4">
+              <InputGroup.Text id="question-input">Q:</InputGroup.Text>
+              <Form.Control
+                placeholder="Type question here"
+                aria-label="Question"
+                aria-describedby="question-input"
+              />
+            </InputGroup>
+
+          <h5>Attachments</h5>
+          <form>
+            <div className="form-group">
+              <input type="file" className="form-control-file" id="exampleFormControlFile1"></input>
+            </div>
+          </form>
+
+          <br></br>
+          <br></br>
+
+          <h5>Response Options</h5>
 
           <InputGroup className="mb-3">
             <InputGroup.Text id="a-input">A:</InputGroup.Text>
@@ -53,11 +88,6 @@ export default function QuestionInput() {
             />
           </InputGroup>
         </div>
-
-        <div className="buttonArea">
-          <Button variant="success">+</Button>
-        </div>
-      </div>
     </div>
     </>
   );
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,15 +1,68 @@
 import QuestionInput from '/pages/QuestionInput'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
+import CloseButton from 'react-bootstrap/CloseButton';
+
 
 export default function Home() {
   return (
     <>
-      <div data-bs-spy="scroll" data-bs-target="#list-example" data-bs-offset="0" className="scrollspy-example">
-        <h4 id="list-item-1">Question 1</h4>
-        <QuestionInput></QuestionInput>
-        <h4 id="list-item-2">Question 2</h4>
-        <QuestionInput></QuestionInput>
-        <h4 id="list-item-3">Question 3</h4>
-        <QuestionInput></QuestionInput>
+      <div className="courseName">ES 2</div>
+      <Tabs
+      defaultActiveKey="profile"
+      id="justify-tab-example"
+      className="mb-3"
+      fill
+      >
+        <Tab eventKey="lectures" title="Lectures">
+        </Tab>
+        <Tab eventKey="roster" title="Roster">
+        </Tab>
+        <Tab eventKey="statistics" title="Statistics">
+        </Tab>
+        <Tab eventKey="class-settings" title="Class Settings">
+        </Tab>
+      </Tabs>
+
+      <div className="lectureDate">Monday, December 13, 2022</div>
+
+      <QuestionInput></QuestionInput>
+
+      <h3 className="live">Live</h3>
+
+      <div className="questionBox">
+        <div className="row">
+          <div className="col1">What is your name?</div>
+          <div className="col2">3/18 answered</div>
+        </div>  
+      </div>
+
+      <h3 className="notLive">Waiting to go live</h3>
+
+      <div className="questionBox">
+        <div className="row">
+          <div className="col1">What is your major?</div>
+          <div className="col2"><CloseButton /></div>
+        </div>  
+      </div>
+      <div className="questionBox">
+        <div className="row">
+          <div className="col1">What year/month are you graduating?</div>
+          <div className="col2"><CloseButton /></div>
+        </div>  
+      </div>
+      <div className="questionBox">
+        <div className="row">
+          <div className="col1">How has your experience in the School of Engineering been?</div>
+          <div className="col2"><CloseButton /></div>
+        </div>  
+      </div>
+      <div className="questionBox">
+        <div className="row">
+          <div className="col1">Name your favorite 3 courses at Tufts.</div>
+          <div className="col2"><CloseButton /></div>
+        </div>  
       </div>
     </>
   );
