a5537a89d6565407c809aaf84316d5b75d43a6a5
Najib Daoud <>
2023-01-30 17:07:14 -0500

    Created parse course id function

--- web/pages/TeacherPage.tsx
+++ web/pages/TeacherPage.tsx
@@ -8,13 +8,17 @@ type TeacherPageProps = {
   course_id: string;
   lecture_id: string;
 }
-
+// TODO: handle renderign before course_id prop is defined
 export default function TeacherPage({course_id, lecture_id}: TeacherPageProps) {
+  var course_name = parse_course_id(course_id)
+
+  console.log("course name: " + course_name)
+  
   return (
     <>
       <h4>Course #{course_id}</h4>
       <h5>Lecture #{lecture_id}</h5>
-      <div className="courseName">ES 2</div>
+      <div className="courseName">{course_name}</div>
       <Tabs
       defaultActiveKey="profile"
       id="justify-tab-example"
@@ -76,4 +80,40 @@ export default function TeacherPage({course_id, lecture_id}: TeacherPageProps) {
       </div>
     </>
   );
+}
+
+// course id must be a string in the form of [characters][digits]
+// e.g. es11, math166
+function parse_course_id(course_id) {
+  if (course_id) {
+    var course_id_len = course_id.length
+    var parsedString = ""
+    var isNum = false
+    if (course_id_len == 0) {
+      throw new Error("INVALID COURSE ID")
+    }
+    for (let i = 0; i < course_id_len; i++) {
+      if ((/[a-zA-Z]/).test(course_id[i])) {
+        if (isNum == true) {
+          throw new Error("INVALID COURSE ID"); 
+        }
+        parsedString = parsedString.concat(course_id[i].toUpperCase())
+      }
+      else if (/^\d$/.test(course_id[i]))  {
+        if (i == 0) {
+          throw new Error("INVALID COURSE ID")
+        }
+        if (isNum == false) {
+          parsedString = parsedString.concat(" ")
+        }
+        isNum = true
+        parsedString = parsedString.concat(course_id[i])
+      }
+      else {
+        throw new Error("INVALID COURSE ID")
+      }
+    }
+  
+    return parsedString
+  }
 }
