1fead6fd5a33581e19cd336b007393f238c7fdac
Najib Daoud <>
2023-02-08 15:23:57 -0500

    move parse_course_id function

--- web/pages/courses/[course_id]/index.tsx
+++ web/pages/courses/[course_id]/index.tsx
@@ -1,13 +1,42 @@
 import { useRouter } from "next/router";
+import parse_course_id from "../../../components/parse_course_id";
 
 const Course = () => {
     const router = useRouter();
     const course_id = router.query.course_id;
 
+    if (typeof course_id != "string") {
+        return <></>
+    }
+
+    var course_name = parse_course_id(course_id)
 
     return (
         <div>
-            <h4>Course #{course_id}</h4>
+            <div className="courseName">{course_name}</div>
+
+            <table className="table table-hover">
+            <thead>
+                <tr>
+                <th scope="col">Unit #</th>
+                <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <th scope="row">1</th>
+                <td>Building a Web Page Overview</td>
+                </tr>
+                <tr>
+                <th scope="row">2</th>
+                <td>Setting Up the Backend</td>
+                </tr>
+                <tr>
+                <th scope="row">3</th>
+                <td>Setting Up the Frontend</td>
+                </tr>
+            </tbody>
+            </table>
         </div>
     )
 }
