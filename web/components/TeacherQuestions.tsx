7b5bfc4ecb7401d8c51c57ea7739389997873f5b
Najib Daoud <>
2023-04-15 17:16:47 -0400

    Adding some statis response functionality. Not properly styled at the moment and not entirely functional.

--- web/components/TeacherQuestions.tsx
+++ web/components/TeacherQuestions.tsx
@@ -4,6 +4,9 @@ import styles from '../styles/TeacherQuestions.module.css';
 import QuestionButton from './QuestionButton';
 import QuestionCard from './QuestionCard';
 import QuestionInput from './QuestionInput';
+import { CloseButton } from 'react-bootstrap';
+import LiveIcon from './svg/LiveIcon';
+
 
 interface ITeacherQuestions {
     liveQuestion: any;
@@ -19,6 +22,23 @@ const TeacherQuestions = ({
     lecture_id,
 }: ITeacherQuestions) => {
     const [addQuestion, setAddQuestion] = useState(false);
+
+    function numberOfResponses(responseChoice: string) {
+        switch (responseChoice) {
+            case "A":
+                console.log("A")
+                return "10"
+            case "B":
+                console.log("B")
+                return "25"
+            case "C":
+                console.log("C")
+                return "3"
+            case "D":
+                console.log("D")
+                return "5"
+        }
+    }
 
     return (
         <div>
@@ -49,6 +69,40 @@ const TeacherQuestions = ({
                 )}
 
             <h3 className={styles.notLive}>Waiting to go live</h3>
+
+            <div className={`card ${styles.cardCustom}`}>
+                <div className='card-body'>
+                    <div className={`row ${styles.questionCardRow1}`}>
+                        <div className={`col-auto `}>
+                            <div className={`row ${styles.questionCardRow2}`}>
+                                <div className={`col `}>
+                                    What is your name?
+                                </div>
+                                <div className={`col ${styles.questionCardCenter}`}>
+                                    <small className="text-muted">5/10 answered</small>
+                                </div>
+                            </div>
+
+                            <div className='row'>
+                                <div className={`col ${styles.questionCardCenter}`}>
+                                    <h3 className="lead"> 
+                                        A: <small className="text-muted"> {numberOfResponses("A")} </small>
+                                        B: <small className="text-muted"> {numberOfResponses("A")} </small>
+                                        C: <small className="text-muted"> {numberOfResponses("A")} </small>
+                                        D: <small className="text-muted"> {numberOfResponses("A")} </small>
+                                    </h3> 
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={`col ${styles.questionCardRight}`}>
+                            <LiveIcon fill='green' />
+                            <CloseButton />
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
 
             {questions &&
                 questions.length > 0 &&
