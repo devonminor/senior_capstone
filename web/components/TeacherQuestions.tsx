1be1e547ae3ed553d201b00812eaf2eff942aa77
Najib Daoud <>
2023-04-17 23:41:56 -0400

    show multiple choice response stats

--- web/components/TeacherQuestions.tsx
+++ web/components/TeacherQuestions.tsx
@@ -19,26 +19,61 @@ const TeacherQuestions = ({
     liveQuestion,
     questions,
     course_id,
-    lecture_id,
+    lecture_id
 }: ITeacherQuestions) => {
     const [addQuestion, setAddQuestion] = useState(false);
+    const [statsVisbility, setStatsVisbility] = useState(false);
 
     function numberOfResponses(responseChoice: string) {
         switch (responseChoice) {
             case "A":
-                console.log("A")
+                console.log("fetch number of answers = A")
                 return "10"
             case "B":
-                console.log("B")
+                console.log("fetch number of answers = B")
                 return "25"
             case "C":
-                console.log("C")
+                console.log("fetch number of answers = C")
                 return "3"
             case "D":
-                console.log("D")
+                console.log("fetch number of answers = D")
                 return "5"
         }
     }
+
+    function handleStatsClick() {
+        console.log("CLICK!")
+        setStatsVisbility(!statsVisbility)
+    }
+
+    const responseStats =
+        <div>
+            <hr></hr>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <h3 className="lead">
+                            A: <small className="text-muted"> {numberOfResponses("A")} </small>
+                        </h3> 
+                    </div>
+                    <div className="col">
+                        <h3 className="lead">
+                            B: <small className="text-muted"> {numberOfResponses("B")} </small>
+                        </h3> 
+                    </div>
+                    <div className="col">
+                        <h3 className="lead">
+                            C: <small className="text-muted"> {numberOfResponses("C")} </small>
+                        </h3> 
+                    </div>
+                    <div className="col">
+                        <h3 className="lead">
+                            D: <small className="text-muted"> {numberOfResponses("D")} </small>
+                        </h3> 
+                    </div>
+                </div>
+            </div>
+        </div>
 
     return (
         <div>
@@ -72,35 +107,36 @@ const TeacherQuestions = ({
 
             <div className={`card ${styles.cardCustom}`}>
                 <div className='card-body'>
-                    <div className={`row ${styles.questionCardRow1}`}>
-                        <div className={`col-auto `}>
-                            <div className={`row ${styles.questionCardRow2}`}>
-                                <div className={`col `}>
+                    <div className={`row ${styles.questionCardContainer}`}>
+                        <div className='col'>
+                            <div className='row'>
+                                <div className={`col ${styles.questionCardQuestion}`}>
                                     What is your name?
                                 </div>
-                                <div className={`col ${styles.questionCardCenter}`}>
+                            </div>
+                            <div className='row'>
+                                <div className={`col ${styles.numAnswered}`}>
                                     <small className="text-muted">5/10 answered</small>
                                 </div>
                             </div>
+                        </div>
 
-                            <div className='row'>
-                                <div className={`col ${styles.questionCardCenter}`}>
-                                    <h3 className="lead"> 
-                                        A: <small className="text-muted"> {numberOfResponses("A")} </small>
-                                        B: <small className="text-muted"> {numberOfResponses("A")} </small>
-                                        C: <small className="text-muted"> {numberOfResponses("A")} </small>
-                                        D: <small className="text-muted"> {numberOfResponses("A")} </small>
-                                    </h3> 
-                                </div>
+                        <div className='col'>
+                            <div className={styles.questionCardIcons}>
+                                <button type="button" className={`btn btn-secondary  ${styles.graphIcon}`} onClick={handleStatsClick}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-bar-chart-line" viewBox="0 0 16 16">
+                                        <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1V2zm1 12h2V2h-2v12zm-3 0V7H7v7h2zm-5 0v-3H2v3h2z"/>
+                                    </svg>
+                                </button>
+                                <LiveIcon fill='green' />
+                                <CloseButton />
                             </div>
                         </div>
 
-                        <div className={`col ${styles.questionCardRight}`}>
-                            <LiveIcon fill='green' />
-                            <CloseButton />
-                        </div>
+                        
                     </div>
-                    
+
+                    {statsVisbility ? responseStats : <></>}
                 </div>
             </div>
 
