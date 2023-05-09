cb1606c19f89174f339f96ffdf04b79661acd16a
Anesu Gavhera <agavhera@gmail.com>
2023-05-09 16:59:52 -0400

    Edited clustering parameters to accept a string array and return a string array

--- backend/analyze.py
+++ backend/analyze.py
@@ -6,8 +6,8 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer 
-from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from scipy.spatial import distance
 from sklearn.preprocessing import StandardScaler
@@ -35,14 +35,12 @@ from kneed import KneeLocator
 # from nltk.tokenize import word_tokenize
 
 
-
 # Steps for clustering student responses
 #   1. Load the data (create a file with a bunch of potential responses to a single question)
 #   2. Process the data (check the spelling of each response, potentially remove stop words, remove punctuation, group like terms (ex. lbs, LBS, pounds), etc.)
 #   3. Vectorize the data (convert the responses into a vector of numbers)
 #   4. Cluster the data (use k-means to cluster the responses into groups)
 #   5. Display the results (print out the clusters and the responses in each cluster)
-
 
 
 # Load the data from file and return array of responses
@@ -53,6 +51,8 @@ def load_data(filename):
     return responses
 
 # Process the data and return array of processed responses
+
+
 def process2_data(responses):
     processed_responses = []
     for response in responses:
@@ -66,6 +66,7 @@ def process2_data(responses):
         print(str(elem) + " -> " + str(y))
     return final_arr
 
+
 def process_data(responses):
     processed_responses = []
     for response in responses:
@@ -74,23 +75,26 @@ def process_data(responses):
     return processed_responses
 
 # Convert all text numbers in a string to numeric numbers
+
+
 def convert_text_numbers_to_numeric_numbers(sentence):
     to_num_arr = []
     for word in sentence.split():
         try:
             conversion = w2n.word_to_num(word)
             if (conversion != None):
-                # loop through remaining words of the sentence 
+                # loop through remaining words of the sentence
                 to_num_arr.append(conversion)
         except:
             to_num_arr.append(word)
             continue
     return (" ".join(str(x) for x in to_num_arr))
 
+
 def standardize_number(text):
-    elems = re.split(r'(\d+)', text) 
+    elems = re.split(r'(\d+)', text)
     split_array = []
-    
+
     count = 0
     while count < len(elems):
         elems[count] = elems[count].strip()
@@ -99,13 +103,15 @@ def standardize_number(text):
                 rounded_num = round(float(elems[count]), 3)
                 split_array.append(str(rounded_num))
                 count += 1
-                
+
             elif elems[count+1] == '.' and elems[count+2].isdigit():
-                rounded_num = round(float("".join([elems[count], elems[count+1], elems[count+2]])), 3)
+                rounded_num = round(
+                    float("".join([elems[count], elems[count+1], elems[count+2]])), 3)
                 split_array.append(str(rounded_num))
                 count += 3
             elif elems[count+1] == '/' and elems[count+2].isdigit():
-                rounded_num = round((float(elems[count])) / (float(elems[count+2])), 3)
+                rounded_num = round(
+                    (float(elems[count])) / (float(elems[count+2])), 3)
                 split_array.append(str(rounded_num))
                 count += 3
             else:
@@ -117,7 +123,8 @@ def standardize_number(text):
             if (len(elems) - 1) <= count:
                 count += 1
             elif elems[count+1].isdigit():
-                rounded_num = round(float("".join([elems[count], elems[count+1]])), 3)
+                rounded_num = round(
+                    float("".join([elems[count], elems[count+1]])), 3)
                 split_array.append(str(rounded_num))
                 count += 2
         else:
@@ -129,15 +136,12 @@ def standardize_number(text):
     return (" ".join(split_array)).strip()
 
 
-    
-
-
 def standardize_number(text):
 
     # split elements via digits
-    elems = re.split(r'(\d+)', text) 
+    elems = re.split(r'(\d+)', text)
     split_array = []
-    
+
     count = 0
     while count < len(elems):
         elems[count] = elems[count].strip()
@@ -153,13 +157,15 @@ def standardize_number(text):
 
             # checks if number is a float
             elif elems[count+1] == '.' and elems[count+2].isdigit():
-                rounded_num = round(float("".join([elems[count], elems[count+1], elems[count+2]])), 3)
+                rounded_num = round(
+                    float("".join([elems[count], elems[count+1], elems[count+2]])), 3)
                 split_array.append(str(rounded_num))
                 count += 3
 
             # checks if number is a fraction
             elif elems[count+1] == '/' and elems[count+2].isdigit():
-                rounded_num = round((float(elems[count])) / (float(elems[count+2])), 3)
+                rounded_num = round(
+                    (float(elems[count])) / (float(elems[count+2])), 3)
                 split_array.append(str(rounded_num))
                 count += 3
 
@@ -172,12 +178,13 @@ def standardize_number(text):
         # if a number is less than 1
         elif elems[count] == '.':
             if (len(elems) - 1) <= count:
-                count += 1;
+                count += 1
             elif elems[count+1].isdigit():
-                rounded_num = round(float("".join([elems[count], elems[count+1]])), 3)
+                rounded_num = round(
+                    float("".join([elems[count], elems[count+1]])), 3)
                 split_array.append(str(rounded_num))
-                count += 2;
-        
+                count += 2
+
         # if parsed string is not a number
         else:
             get_string = re.sub('[^A-Za-z0-9]+', " ", elems[count])
@@ -188,67 +195,72 @@ def standardize_number(text):
     return (" ".join(split_array)).strip()
 
 
-
-
-# main
-if __name__ == "__main__":
-
-    print("Welcome to my Algorithm")
-
+def beginClustering(words_list_param=None):
     # Initialize tweeker
     tk = TweetTokenizer()
     count_vectorizer = CountVectorizer(stop_words=[], tokenizer=tk.tokenize)
-   
-    
-    ## Creating a data frame to represent the number of the words in every sentence
-    unprocessed_responses = load_data("./text_responses_bell3.txt")
+
+    # Creating a data frame to represent the number of the words in every sentence
+    # unprocessed_responses = load_data(
+    #    "./cluster_example_data/text_responses_bell3.txt")
+
+    unprocessed_responses = []
+
+    words_list = []
+    if words_list_param is None:
+        print("No list provided!")
+        loaded_data = load_data(
+            "./cluster_example_data/text_responses_bell3.txt")
+        for word in loaded_data:
+            words_list.append(word)
+    else:
+        for word in words_list_param:
+            words_list.append(word)
+
+    # Comment this out for devon, and rename param to words_list
+
+    # Add words_list to unprocessed_ data
+    for word in words_list:
+        unprocessed_responses.append(word)
+
     responses = process_data(unprocessed_responses)
 
-    final_arr = []
+    processed_data = []
     for elem in responses:
         x = convert_text_numbers_to_numeric_numbers(elem)
         y = standardize_number(x)
-        final_arr.append(y)
+        processed_data.append(y)
 
-    print(final_arr)
-
-    matrix = count_vectorizer.fit_transform(final_arr)
+    matrix = count_vectorizer.fit_transform(processed_data)
     table = matrix.todense()
     df = pd.DataFrame(table, columns=count_vectorizer.get_feature_names_out())
 
-
-    ## Aplying the Cosine similarity module 
+    # Aplying the Cosine similarity module
     values = cosine_similarity(df, df)
     df = pd.DataFrame(values)
 
-
-    ## Applying the Euclidean Distance
+    # Applying the Euclidean Distance
     matrix2 = distance.cdist(df, df, 'euclidean')
     df_eucl = pd.DataFrame(matrix2)
 
-    ## Standardization
-    # scaler = StandardScaler()
-    # scaled_features = scaler.fit_transform(df)
-
     # Varying the number of clusters and to see what the optimum k is
     kmeans_kwargs = {
-    "init": "random",
-    "n_init": 10,
-    "max_iter": 300,
+        "init": "random",
+        "n_init": 10,
+        "max_iter": 300,
     }
 
     # A list holds the SSE values for each k. Try for each k 1 - 10 and compare SSE values
     sse = []
     for k in range(1, 10):
-        kmeans = KMeans(n_clusters = k, **kmeans_kwargs)
+        kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
         kmeans.fit(df)
         sse.append(kmeans.inertia_)
         cluster_labels = kmeans.labels_
-        silhouette_avg=[]
+        silhouette_avg = []
 
     cluster_labels = kmeans.labels_
-    silhouette_avg=[]
- 
+    silhouette_avg = []
 
     # Calculate true k from the results
     kl = KneeLocator(range(1, 10), sse, curve="convex", direction="decreasing")
@@ -259,41 +271,123 @@ if __name__ == "__main__":
     kmeans = KMeans(init="random", n_clusters=true_k, n_init=10, max_iter=300)
     kmeans.fit(df)
     predict = kmeans.fit_predict(df)
-    #print("Array of clusters:")
-    #print(predict)
 
     # Write COSINE results to file
-    fp = input_file = open("Clusters_processed_+3_clusters_bell3.txt", "w")
-    fp.write("Results Using Cosine Function \n")
+    final_result = []
     for i in range(0, 10):
-        fp.write("Cluster: " + str(i) + " --> ")
+        cluster = []
         for x in range(len(responses)):
-            if predict[x] == i: 
-                fp.write(responses[x])
-                fp.write(" | ")
-        fp.write("\n")
-        
+            if predict[x] == i:
+                # fp.write(responses[x])
+                # fp.write(" | ")
+                cluster.append(responses[x])
+        if (len(cluster) != 0):
+            final_result.append(cluster)
+
+    return final_result
 
 
-     # ACTUAL K means - EUCL
-    kmeans = KMeans(init="random", n_clusters=true_k, n_init=10, max_iter=300)
-    kmeans.fit(df_eucl)
-    predict = kmeans.fit_predict(df_eucl)
-    #print("Array of clusters:")
-    #print(predict)
+# main
+if __name__ == "__main__":
 
-     # Write EUCL results to file
-    fp.write("\n")
-    fp.write("Results Using Eucl Function \n")
-    for i in range(0, 10):
-        fp.write("Cluster: " + str(i) + " --> ")
-        for x in range(len(responses)):
-            if predict[x] == i: 
-                fp.write(responses[x])
-                fp.write(" | ")
-        fp.write("\n")
-    fp.close()
-    
+    print("Welcome to my Algorithm")
 
+    raw_responses = ['<(O)>/ \\<(O)>', '8.3x10^-4 V', '5/6 V', ':)', '.83V', '0.83v', '5/6', '0.83v', '5/12000',
+                     '8.333 V', '5/6 volt', '5/6 V', '4.16', '4.16 V', '10/12 Volts', '4.16', '10/12 v', '5/6 V ~ 0.833 V',
+                     '.83v', '5/12000A', '5/6 volts', '5/6 volts', '5 V']
 
+    final_result = beginClustering(raw_responses)
 
+    # # Initialize tweeker
+    # tk = TweetTokenizer()
+    # count_vectorizer = CountVectorizer(stop_words=[], tokenizer=tk.tokenize)
+
+    # # Creating a data frame to represent the number of the words in every sentence
+    # unprocessed_responses = load_data(
+    #     "./cluster_example_data/text_responses_bell3.txt")
+    # responses = process_data(unprocessed_responses)
+
+    # final_arr = []
+    # for elem in responses:
+    #     x = convert_text_numbers_to_numeric_numbers(elem)
+    #     y = standardize_number(x)
+    #     final_arr.append(y)
+
+    # print(final_arr)
+
+    # matrix = count_vectorizer.fit_transform(final_arr)
+    # table = matrix.todense()
+    # df = pd.DataFrame(table, columns=count_vectorizer.get_feature_names_out())
+
+    # # Aplying the Cosine similarity module
+    # values = cosine_similarity(df, df)
+    # df = pd.DataFrame(values)
+
+    # # Applying the Euclidean Distance
+    # matrix2 = distance.cdist(df, df, 'euclidean')
+    # df_eucl = pd.DataFrame(matrix2)
+
+    # # Standardization
+    # # scaler = StandardScaler()
+    # # scaled_features = scaler.fit_transform(df)
+
+    # # Varying the number of clusters and to see what the optimum k is
+    # kmeans_kwargs = {
+    #     "init": "random",
+    #     "n_init": 10,
+    #     "max_iter": 300,
+    # }
+
+    # # A list holds the SSE values for each k. Try for each k 1 - 10 and compare SSE values
+    # sse = []
+    # for k in range(1, 10):
+    #     kmeans = KMeans(n_clusters=k, **kmeans_kwargs)
+    #     kmeans.fit(df)
+    #     sse.append(kmeans.inertia_)
+    #     cluster_labels = kmeans.labels_
+    #     silhouette_avg = []
+
+    # cluster_labels = kmeans.labels_
+    # silhouette_avg = []
+
+    # # Calculate true k from the results
+    # kl = KneeLocator(range(1, 10), sse, curve="convex", direction="decreasing")
+    # print("True K: " + str(kl.elbow))
+    # true_k = kl.elbow + 3
+
+    # # ACTUAL K means - COSINE
+    # kmeans = KMeans(init="random", n_clusters=true_k, n_init=10, max_iter=300)
+    # kmeans.fit(df)
+    # predict = kmeans.fit_predict(df)
+    # # print("Array of clusters:")
+    # # print(predict)
+
+    # # Write COSINE results to file
+    # fp = input_file = open("Clusters_processed_+3_clusters_bell3.txt", "w")
+    # fp.write("Results Using Cosine Function \n")
+    # for i in range(0, 10):
+    #     fp.write("Cluster: " + str(i) + " --> ")
+    #     for x in range(len(responses)):
+    #         if predict[x] == i:
+    #             fp.write(responses[x])
+    #             fp.write(" | ")
+    #     fp.write("\n")
+
+    #  # ACTUAL K means - EUCL
+    # kmeans = KMeans(init="random", n_clusters=true_k, n_init=10, max_iter=300)
+    # kmeans.fit(df_eucl)
+    # predict = kmeans.fit_predict(df_eucl)
+    # # print("Array of clusters:")
+    # # print(predict)
+
+    # # Write EUCL results to file
+    # fp.write("\n")
+    # fp.write("Results Using Eucl Function \n")
+    # for i in range(0, 10):
+    #     fp.write("Cluster: " + str(i) + " --> ")
+    #     for x in range(len(responses)):
+    #         if predict[x] == i:
+    #             fp.write(responses[x])
+    #             fp.write(" | ")
+    #     fp.write("\n")
+    # fp.close()
